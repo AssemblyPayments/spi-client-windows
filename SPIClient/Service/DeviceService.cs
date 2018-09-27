337aa4fa5e768f5ea7ca9ac2a8324f302459f8ce
Raymond <Raysta@gmail.com>
2018-09-27 20:30:32 +1000

    Add device service helper

---
+++ SPIClient/Service/DeviceService.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+
+namespace SPIClient.Service
+{
+    public class DeviceService
+    {
+        public string Ip { get; set; }
+        public string Last_updated { get; set; }
+    }
+
+    public class DeviceIpService
+    {
+        public DeviceService RetrieveService(string serialNumber)
+        {
+            var deviceIpUrl = $"https://device-address-api-dev.nonprod-wbc.msp.assemblypayments.com/v1/{serialNumber}/ip";
+            var ipService = new RestServiceHelper(deviceIpUrl);
+            var request = new RestRequest(Method.GET);
+            var response = ipService.SendRequest<DeviceService>(request);
+
+            return response;
+        }
+    }
+}
---
+++ SPIClient/Service/IRestServiceHelper.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+namespace SPIClient.Service
+{
+    public interface IRestServiceHelper
+    {
+        T SendRequest<T>(IRestRequest request) where T : new();
+    }
+}
