d4b869ca328f9aec0615800d2eed2ee893fa0d39
Metin Avci <mavci@assemblypayments.com>
2019-01-08 11:16:30 +1100

    SCLN-60 Apply disable pay at table on 2.1.9

--- SPIClient/Spi.cs
+++ SPIClient/Spi.cs
@@ -123,6 +123,13 @@ namespace SPIClient
             return _spiPat;
         }
 
+        public SpiPayAtTable DisablePayAtTable()
+        {
+            _spiPat = new SpiPayAtTable(this);
+            _spiPat.Config.PayAtTabledEnabled = false;
+            return _spiPat;
+        }
+
         public SpiPreauth EnablePreauth()
         {
             _spiPreauth = new SpiPreauth(this, _txLock);
