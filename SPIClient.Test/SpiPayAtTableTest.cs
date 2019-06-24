35729a58e7b002cd45b0f692d7a0061d33d8aeef
Metin Avci <mavci@assemblypayments.com>
2019-06-25 09:32:57 +1000

    Fixing test case in SpiPayAtTableTest

--- SPIClient.Test/SpiPayAtTableTest.cs
+++ SPIClient.Test/SpiPayAtTableTest.cs
@@ -54,7 +54,7 @@ namespace Test
 
             JArray getOpenTablesArray = (JArray)m.Data["tables"];
             List<OpenTablesEntry> getOpenTablesList = getOpenTablesArray.ToObject<List<OpenTablesEntry>>();
-            Assert.Equal(openTablesEntries.ToArray(), openTablesEntries.ToArray());
+            Assert.Equal(openTablesEntries.Count, getOpenTablesList.Count);
             Assert.Equal(openTablesEntries.Count, 2);
         }
 
