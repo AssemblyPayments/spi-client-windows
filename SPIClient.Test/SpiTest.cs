c189095e5ac05d7f07a5443ee7156afc04787ff4
Raymond <Raysta@gmail.com>
2019-08-28 13:56:29 +1000

    Update Unit tests

--- SPIClient.Test/SpiTest.cs
+++ SPIClient.Test/SpiTest.cs
@@ -41,11 +41,8 @@ namespace Test
         {
             // arrange
             const string posId = "12345678901234567";
-            const int lengthOfPosId = 16;
-            var spi = new Spi();
-            var messageStamp = new MessageStamp("", null, new System.TimeSpan());
-            SpiClientTestUtils.SetInstanceField(spi, "_currentStatus", SpiStatus.Unpaired);
-            SpiClientTestUtils.SetInstanceField(spi, "_spiMessageStamp", messageStamp);
+            const string eftposAddress = "10.20.30.40";
+            var spi = new Spi(posId, "", eftposAddress,null);
 
             // act
             spi.SetPosId(posId);
@@ -53,36 +50,30 @@ namespace Test
 
             // assert
             Assert.NotEqual(posId, value);
-            Assert.Equal(lengthOfPosId, value.ToString().Length);
         }
 
         [Fact]
-        public void SpiInitate_OnInvalidLengthForPosId_IsSet()
+        public void SpiInitiate_OnInvalidLengthForPosId_IsSet()
         {
             // arrange
             const string posId = "12345678901234567";
-            const int lengthOfPosId = 16;
-            var spi = new Spi(posId, "", "", null);
-            SpiClientTestUtils.SetInstanceField(spi, "_currentStatus", SpiStatus.Unpaired);
+            const string eftposAddress = "10.20.30.40";
+            var spi = new Spi(posId, "", eftposAddress, null);
 
             // act            
             var value = SpiClientTestUtils.GetInstanceField(typeof(Spi), spi, "_posId");
 
             // assert
             Assert.NotEqual(posId, value);
-            Assert.Equal(lengthOfPosId, value.ToString().Length);
         }
 
         [Fact]
         public void SetPosId_OnValidCharacters_IsSet()
         {
             // arrange
-            const string posId = "RamenPos@";
-            var regexItemsForPosId = new Regex("^[a-zA-Z0-9 ]*$");
-            var spi = new Spi();
-            var messageStamp = new MessageStamp("", null, new System.TimeSpan());
-            SpiClientTestUtils.SetInstanceField(spi, "_currentStatus", SpiStatus.Unpaired);
-            SpiClientTestUtils.SetInstanceField(spi, "_spiMessageStamp", messageStamp);
+            const string posId = "RamenPos";
+            const string eftposAddress = "10.20.30.40";
+            var spi = new Spi(posId, "", eftposAddress, null);
 
             // act
             spi.SetPosId(posId);
@@ -90,34 +81,28 @@ namespace Test
 
             // assert
             Assert.Equal(posId, value);
-            Assert.False(regexItemsForPosId.IsMatch(posId));
-            Assert.Equal(regexItemsForPosId.IsMatch(posId), regexItemsForPosId.IsMatch(value.ToString()));
         }
 
         [Fact]
-        public void SpiInitate_OnValidCharactersForPosId_IsSet()
+        public void SpiInitiate_OnValidCharactersForPosId_IsSet()
         {
             // arrange
             const string posId = "RamenPos@";
-            var regexItemsForPosId = new Regex("^[a-zA-Z0-9 ]*$");
-            var spi = new Spi(posId, "", "", null);
-            SpiClientTestUtils.SetInstanceField(spi, "_currentStatus", SpiStatus.Unpaired);
+            const string eftposAddress = "10.20.30.40";
 
             // act
+            var spi = new Spi(posId, "", eftposAddress, null);
             var value = SpiClientTestUtils.GetInstanceField(typeof(Spi), spi, "_posId");
 
             // assert
-            Assert.Equal(posId, value);
-            Assert.False(regexItemsForPosId.IsMatch(posId));
-            Assert.Equal(regexItemsForPosId.IsMatch(posId), regexItemsForPosId.IsMatch(value.ToString()));
+            Assert.NotEqual(posId, value);
         }
 
         [Fact]
         public void SetEftposAddress_OnValidCharacters_IsSet()
         {
             // arrange
-            const string eftposAddress = "10.20";
-            var regexItemsForEftposAddress = new Regex(@"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$");
+            const string eftposAddress = "10.20.30.40";
             var spi = new Spi();
             var conn = new Connection();
             SpiClientTestUtils.SetInstanceField(spi, "_currentStatus", SpiStatus.Unpaired);
@@ -125,31 +110,25 @@ namespace Test
 
             // act
             spi.SetEftposAddress(eftposAddress);
-            var value = SpiClientTestUtils.GetInstanceField(typeof(Spi), spi, "_eftposAddress");
-            value = value.ToString().Remove(0, 5);
+            var value = SpiClientTestUtils.GetInstanceField(typeof(Spi), spi, "_eftposAddress").ToString().Replace("ws://", "");
 
             // assert
-            Assert.False(regexItemsForEftposAddress.IsMatch(eftposAddress));
             Assert.Equal(eftposAddress, value);
-            Assert.Equal(regexItemsForEftposAddress.IsMatch(eftposAddress), regexItemsForEftposAddress.IsMatch(value.ToString()));
         }
 
         [Fact]
         public void SpiInitate_OnValidCharactersForEftposAddress_IsSet()
         {
             // arrange
-            const string eftposAddress = "10.20";
-            var regexItemsForEftposAddress = new Regex(@"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$");
-            var spi = new Spi("", eftposAddress, "", null);
-            SpiClientTestUtils.SetInstanceField(spi, "_currentStatus", SpiStatus.Unpaired);
+            const string eftposAddress = "10.20.30.40";
+            const string posId = "DummyPos";
 
-            var value = SpiClientTestUtils.GetInstanceField(typeof(Spi), spi, "_eftposAddress");
-            value = value.ToString().Remove(0, 5);
+            // act
+            var spi = new Spi(posId, "", eftposAddress, null);
+            var value = SpiClientTestUtils.GetInstanceField(typeof(Spi), spi, "_eftposAddress").ToString().Replace("ws://", ""); 
 
             // assert
-            Assert.False(regexItemsForEftposAddress.IsMatch(eftposAddress));
-            Assert.NotEqual(eftposAddress, value);
-            Assert.Equal(regexItemsForEftposAddress.IsMatch(eftposAddress), regexItemsForEftposAddress.IsMatch(value.ToString()));
+            Assert.Equal(eftposAddress, value);
         }
 
         [Fact]
