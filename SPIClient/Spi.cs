ce7f66f4bb888a88256aedd6403b6a70a4a98643
Raymond Ko <rko@assemblypayments.com>
2019-11-12 10:13:50 +1100

    SP-361 - GLT for a surcharge transaction during network drop

--- SPIClient/Spi.cs
+++ SPIClient/Spi.cs
@@ -1053,7 +1053,7 @@ namespace SPIClient
         }
 
         /// <summary>
-        /// See GltMatch. VSV-2277 This prevents issue with PosRefId associated with the wrong transaction
+        /// See GltMatch. VSV-2277 This prevents issue with PosRefId associated with the wrong transaction - for purchase only.
         /// </summary>
         /// <param name="gltResponse">The GetLastTransactionResponse message to check</param>
         /// <param name="posRefId">The Reference Id that you passed in with the original request</param>
@@ -1072,7 +1072,7 @@ namespace SPIClient
                 return Message.SuccessState.Unknown;
             }
 
-            if (gltResponse.GetTxType().ToUpper() == "PURCHASE" && gltResponse.GetBankNonCashAmount() != expectedAmount && compare > 0)
+            if (gltResponse.GetTxType().ToUpper() == "PURCHASE" && gltResponse.GetPurchaseAmount() != expectedAmount && compare > 0)
             {
                 return Message.SuccessState.Unknown;
             }
